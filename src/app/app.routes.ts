560a9eeb41d5983de5a4f3507a2454cdca94995b
DESKTOP-8TDMRTH\Ethan <etreece3@gmail.com>
2026-01-18 23:37:13 -0500

    add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { ButtonPage } from './pages/button-page/button-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'BungieNet-ComponentLibrary',
+    pathMatch: 'full'
+  },
+  {
+    path: 'BungieNet-ComponentLibrary',
+    children: [
+      {
+        path: 'buttons',
+        component: ButtonPage
+      },
+      // Add more component pages here
+      {
+        path: '',
+        redirectTo: 'buttons',
+        pathMatch: 'full'
+      }
+    ]
+  },
+  {
+    path: '**',
+    redirectTo: 'BungieNet-ComponentLibrary'
+  }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ButtonPage } from "./pages/button-page/button-page";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ButtonPage],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
