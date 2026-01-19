caf1f19b113737d382d6a93ce0f385dfada4d4f8
DESKTOP-8TDMRTH\Ethan <etreece3@gmail.com>
2026-01-18 19:26:02 -0500

    add material theme, fix deploy to gh-pages

--- src/app/pages/button-page/button-page.ts
+++ src/app/pages/button-page/button-page.ts
@@ -1,12 +1,19 @@
-import { Component } from "@angular/core";
+import { Component, signal } from "@angular/core";
 import { HaloForumButton } from "../../shared/components/halo-forum-button/halo-forum-button";
+import { MatSlideToggle } from "@angular/material/slide-toggle";
+import { MatTooltipModule} from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-button-page',
-  imports: [HaloForumButton],
+  imports: [HaloForumButton, MatSlideToggle, MatTooltipModule],
   templateUrl: './button-page.html',
   styleUrl: './button-page.css',
 })
 export class ButtonPage {
 
+  defaultVisible = signal(true);
+
+  toggleDefaultVisible(): void {
+    this.defaultVisible.set(!this.defaultVisible());
+  }
 };
